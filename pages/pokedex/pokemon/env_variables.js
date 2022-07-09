609de87cc823bdd2fecbcf590ecc3fdbb5a5103f
Magdalena Koziol <magda@Magdalenas-MacBook-Air.local>
2022-07-09 10:49:48 +0200

    files deleted

--- pages/pokedex/pokemon/env_variables.js
+++
@@ -1,3 +0,0 @@
-export const publicApi = process.env.NEXT_PUBLIC_API;
-export const pokemonImgApi = process.env.NEXT_PUBLIC_POKEMON_IMG;
-export const storageApi = process.env.NEXT_PUBLIC_STORAGE_API;
