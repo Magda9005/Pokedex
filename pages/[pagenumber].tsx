77d2296c1404da1ea74d77ad1047824b35d84bc6
Magdalena Koziol <magda@Magdalenas-MacBook-Air.local>
2022-12-18 15:46:22 +0100

    correcting ts error

--- pages/[pagenumber].tsx
+++ pages/[pagenumber].tsx
@@ -26,7 +26,7 @@ const PokemonsPage: React.FC<PokemonsPageProps> = ({
         <title>Pokedex </title>
       </Head>
       <PokemonPage
-        pageNr={router.query.pagenumber.toString()}
+        pageNr={Number(router.query.pagenumber)}
         pokemons={pokemons}
         pokemonsWithTypes={pokemonsWithTypes}
         allNames={allNames}
